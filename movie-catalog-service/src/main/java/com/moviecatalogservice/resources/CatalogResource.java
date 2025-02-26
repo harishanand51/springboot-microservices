3c254fe3044d0db7c77a48c786d1efe98e16611f
harishanand51 <harishanand51@gmail.com>
2025-02-26 00:45:20 -0500

    added movie catalog,movie info,ratings info APIs

---
+++ movie-catalog-service/src/main/java/com/moviecatalogservice/resources/CatalogResource.java
@@ -0,0 +1,22 @@
+package com.moviecatalogservice.resources;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.moviecatalogservice.models.CatalogItem;
+
+@RestController
+@RequestMapping("/catalog")
+public class CatalogResource {
+	
+	@RequestMapping("/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+		
+		return Collections.singletonList(new CatalogItem("Test", "Test Desc", 4));
+	}
+
+}
